b3850069211cb195ea32ba4ddef0db787d680a4f
Sam <skacir@brynmawr.edu>
2017-04-14 13:40:19 -0400

    initial commit

---
+++ displayAddress.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# displayAddress.sh
+
+python3.4 /home/pi/printIP.py &
---
+++ launchJupyterServer.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# launchJupyterServer.sh
+
+/usr/local/bin/jupyter notebook --config='/home/pi/.jupyter/jupyter_notebook_config.py' &
+
